e66dbf1b4669ce47d7b008b154dcf768bed21b03
neytoe <nanyankah@gmail.com>
2022-05-20 19:35:08 +0100

    change Db to inmemory

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -7,9 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<DataContext>(options =>
-                options.UseSqlite(
-                        builder.Configuration.GetConnectionString("DbConn")));
+//builder.Services.AddDbContext<DataContext>(options =>
+//                options.UseSqlite(
+//                        builder.Configuration.GetConnectionString("DbConn")));
+builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Inmemory"));
 
 builder.Services.AddScoped<IProductRepo, ProductRepo>();
 builder.Services.AddScoped<IInventoryRepo, InventoryRepo>();   
